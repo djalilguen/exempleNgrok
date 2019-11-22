603b3631ee1a42583b4f2368fbeabbe97902df9c
Admin <Admin@10.0.2.15>
2019-11-22 20:33:38 +0100

    Merci

--- src/exemplengok/Main.java
+++ src/exemplengok/Main.java
@@ -17,6 +17,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Bonjour"+args[0]+", "+args[1]);
         System.out.println("Au revoir djalil, bonne nuit");
+        System.out.println("Merci");
     }
     
 }
